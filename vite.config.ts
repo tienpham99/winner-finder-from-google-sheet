a211e3cd9bdd426ca21ef8f6a4786bf14ca77370
Phạm Tiến <tienpham89.99@gmail.com>
2025-10-04 16:13:21 +0700

    vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/winner-finder-from-google-sheet/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -19,6 +19,11 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: false,
+        minify: 'terser'
       }
     };
 });
